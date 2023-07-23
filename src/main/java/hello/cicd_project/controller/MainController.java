d6127bf019107630a379649be8692b8a23090153
sds2317884 <lee2317884@naver.com>
2023-07-23 22:26:43 +0900

    update

--- src/main/java/hello/cicd_project/controller/MainController.java
+++ src/main/java/hello/cicd_project/controller/MainController.java
@@ -8,6 +8,6 @@ public class MainController {
 
     @GetMapping("/hello")
     public String helloWorld() {
-        return "Hello my name is Hugh!";
+        return "finally successful.";
     }
 }
