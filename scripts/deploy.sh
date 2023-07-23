fe0ce0db4c791af16cf49f9afa4a8cff6a1adf28
sds2317884 <lee2317884@naver.com>
2023-07-23 21:35:25 +0900

    update

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -1,26 +1,22 @@
 #!/usr/bin/env bash
 
-BUILD_JAR=$(ls /home/ec2-user/cicdproject/build/libs/*.jar)
-JAR_NAME=$(basename $BUILD_JAR)
-echo "> build 파일명: $JAR_NAME" >> /home/ec2-user/cicdproject/deploy.log
+REPOSITORY=/home/ec2-user/cicdproject
+cd $REPOSITORY
 
-echo "> build 파일 복사" >> /home/ec2-user/cicdproject/deploy.log
-DEPLOY_PATH=/home/ec2-user/sictak/
-cp $BUILD_JAR $DEPLOY_PATH
+APP_NAME=cicdproject
+JAR_NAME=$(ls $REPOSITORY/build/libs/ | grep 'SNAPSHOT.jar' | tail -n 1)
+JAR_PATH=$REPOSITORY/build/libs/$JAR_NAME
 
-echo "> 현재 실행중인 애플리케이션 pid 확인" >> /home/ec2-user/cicdproject/deploy.log
-CURRENT_PID=$(pgrep -f $JAR_NAME)
+CURRENT_PID=$(pgrep -f $APP_NAME)
 
 if [ -z $CURRENT_PID ]
 then
-  echo "> 현재 구동중인 애플리케이션이 없으므로 종료하지 않습니다." >> /home/ec2-user/cicdproject/deploy.log
+  echo "> 종료할것 없음."
 else
-  echo "> kill -15 $CURRENT_PID"
+  echo "> kill -9 $CURRENT_PID"
   kill -15 $CURRENT_PID
   sleep 5
 fi
 
-DEPLOY_JAR=$DEPLOY_PATH$JAR_NAME
-echo "> DEPLOY_JAR 배포"    >> /home/ec2-user/cicdproject/deploy.log
-chmod +x $DEPLOY_JAR
-nohup java -jar $DEPLOY_JAR >> /home/ec2-user/deploy.log 2>/home/ec2-user/cicdproject/deploy_err.log &
+echo "> $JAR_PATH 배포"
+nohup java -jar $JAR_PATH > /dev/null 2> /dev/null < /dev/null &
